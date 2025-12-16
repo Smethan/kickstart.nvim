dc1e22f4716546edcc6d0361e749c92cd233525b
smethan <ethanhargus@gmail.com>
2025-12-15 18:44:05 -0600

    updated plugins and init.lua config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,56 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'sudo-tee/opencode.nvim',
+    config = function()
+      require('opencode').setup {}
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          anti_conceal = { enabled = false },
+          file_types = { 'markdown', 'opencode_output' },
+        },
+        ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
+      },
+      -- Optional, for file mentions and commands completion, pick only one
+      'saghen/blink.cmp',
+      -- 'hrsh7th/nvim-cmp',
+
+      -- Optional, for file mentions picker, pick only one
+      'folke/snacks.nvim',
+      -- 'nvim-telescope/telescope.nvim',
+      -- 'ibhagwan/fzf-lua',
+      -- 'nvim_mini/mini.nvim',
+    },
+  },
+  {
+    'supermaven-inc/supermaven-nvim',
+    event = 'VeryLazy', -- Load after Neovim starts
+    config = function()
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<Tab>',
+          clear_suggestion = '<C-]>',
+          accept_word = '<C-j>',
+        },
+        log_level = 'info',
+        disable_inline_completion = false,
+        disable_keymaps = false,
+        condition = function()
+          return false
+        end,
+      }
+      -- Link suggestion highlight to Comment for theme-derived color
+      vim.api.nvim_set_hl(0, 'SupermavenSuggestion', { link = 'Comment' })
+    end,
+  },
+  {
+    'christoomey/vim-tmux-navigator',
+    event = 'VeryLazy',
+  },
+}
